0cd4418499ff941edd6280ac7528f86d4603678c
Richie <280645618@qq.com>
2025-01-12 12:21:19 +0800

    新增图片上传功能

---
+++ app/src/main/java/cn/qihangerp/app/controller/UploadImageResult.java
@@ -0,0 +1,16 @@
+package cn.qihangerp.app.controller;
+
+import lombok.Data;
+
+/**
+ * 描述：
+ * 上传图片返回
+ *
+ * @author qlp
+ * @date 2018-12-20 5:23 PM
+ */
+@Data
+public class UploadImageResult {
+    private String url;
+    private String name;
+}
