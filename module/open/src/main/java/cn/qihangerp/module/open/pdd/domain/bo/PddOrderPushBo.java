d8991ac21eff5ff1bcd974a8d7bebc8a16239c33
Richie <280645618@qq.com>
2025-04-23 11:55:27 +0800

    优化新脚骨

--- module/open/src/main/java/cn/qihangerp/module/open/pdd/domain/bo/PddOrderPushBo.java
+++
@@ -1,8 +0,0 @@
-package cn.qihangerp.module.open.pdd.domain.bo;
-
-import lombok.Data;
-
-@Data
-public class PddOrderPushBo {
-    private String[] ids;
-}
--- module/open/src/main/java/cn/qihangerp/module/open/wei/domain/bo/WeiOrderPushBo.java
+++
@@ -1,8 +0,0 @@
-package cn.qihangerp.module.open.wei.domain.bo;
-
-import lombok.Data;
-
-@Data
-public class WeiOrderPushBo {
-    private String[] ids;
-}
