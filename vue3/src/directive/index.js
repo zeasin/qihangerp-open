11fc8bd4ff87778c5105402524f090637765205e
启航老齐 <280645618@qq.com>
2025-09-15 09:44:59 +0800

    升级

--- vue3/src/directive/index.js
+++
@@ -1,7 +0,0 @@
-import { hasPerm } from "./permission";
-
-// 全局注册 directive
-export function setupDirective(app) {
-  // 使 v-hasPerm 在所有组件中都可用
-  app.directive("hasPerm", hasPerm);
-} 
--- vue3/src/plugins/icons.js
+++
@@ -1,8 +0,0 @@
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-
-// 注册所有图标
-export function setupElIcons(app) {
-  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-  }
-} 
